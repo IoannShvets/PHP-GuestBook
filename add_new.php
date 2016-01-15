eaf327296dd78955a8e46bca5e392f990c3673d2
IoannShvets <shvets.ioann2015@yandex.ru>
2016-01-15 19:40:50 +0300

    Update add_new.php

--- add_new.php
+++ add_new.php
@@ -1,17 +1,23 @@
 <html>
 <head>
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <style>
+        form {
+        	text-align: center;
+        }
+        </style>
 </head>
 <body>
 	<form action="success.php" method="post" name="form">
 		<p><b>Ваше имя:</b><br>
-		<input type="text" name="name" size="40">
-		<p> <b>Отзыв</b><br>
-		<textarea name="content" cols="40" rows="3"></textarea></p>
+		<input type="text" name="name" size="40" required>
+		<p><b>Ваш сайт:</b><br>
+		<input type="text" name="site" size="40">
+		<p><b>Отзыв</b><br>
+		<textarea name="content" cols="40" rows="10" required></textarea></p>
 		<img src = "captcha.php" />
-		<input type = "text" name = "kapcha" />
-		<input id="submit" type="submit" value="Добавить"><br/>
+		<input type = "text" name = "kapcha" required/>
+		<p><input id="submit" type="submit" value="Добавить"><br/>
 	</form>
 </body>
 </html>
- 
